568f312766f21613769dba605d71392a3fb47e46
rahmadantijp17 <44959173+rahmadantijp17@users.noreply.github.com>
2023-08-03 22:45:48 +0700

    Add files via upload

---
+++ program_perpustakaan.sql
@@ -0,0 +1,11 @@
+create table buku
+(
+    id        int auto_increment
+        primary key,
+    nama_buku varchar(50) null,
+    penulis   varchar(50) null,
+    penerbit  varchar(50) null,
+    jumlah    int         null
+);
+
+
